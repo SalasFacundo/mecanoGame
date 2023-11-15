e560f1f53c8b60f2a4151d0ee9b68335b798fc19
Facundo <salasfacundo96@gmail.com>
2023-11-15 18:40:31 -0300

    configuracion angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { ScreenComponent } from './components/screen/screen.component';
+import { DataBarComponent } from './components/data-bar/data-bar.component';
+import { GameOverModalComponent } from './components/modals/game-over-modal/game-over-modal.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    ScreenComponent,
+    DataBarComponent,
+    GameOverModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
